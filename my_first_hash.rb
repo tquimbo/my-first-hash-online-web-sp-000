13abfde5bf47448a58a1c24dd9b689e5febced45
Tim Quimbo <tquimbo1@gmail.com>
2021-03-01 23:36:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,12 +19,12 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-
+  # shipping_manifest = {
+  #                 "whale bone corsets" => 5,
+  #                 "porcelain vases" => 2,
+  #                 "oil paintings" => 3
+  #                }
+shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
