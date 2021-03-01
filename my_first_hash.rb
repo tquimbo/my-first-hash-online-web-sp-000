95373d9df14962c2e36cf2b986ae84594c86a3e9
Tim Quimbo <tquimbo1@gmail.com>
2021-03-01 23:49:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,11 +37,11 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-shipping_manifest["muskets"] = "2"
+shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-shipping_manifest["gun poweder"] = "4"
+shipping_manifest["gun poweder"] = 4
 
   # return the shipping_manifest hash below
 return shipping_manifest
